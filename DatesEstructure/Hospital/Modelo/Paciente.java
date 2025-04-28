d2733f83785ee37dcc522609d796ae4e4f3ecb59
unknown <danielmedcalfe772@gmail.com>
2025-04-28 11:18:35 -0500

    paciente casi listo

---
+++ DatesEstructure/Hospital/Modelo/Paciente.java
@@ -0,0 +1,44 @@
+package Modelo;
+
+public class Paciente {
+    private String id;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String genero;
+    private String direccion;
+    private String telefono;
+    private String historialMedico;
+
+    public Paciente(String id, String nombre, String apellido,int edad,String genero, String direccion, String telefono){
+      this.id=id ;
+      this.nombre=nombre;
+      this.apellido=apellido;
+      this.edad=edad;
+      this.genero=genero;
+      this.direccion=direccion;
+      this.telefono=telefono;
+      this.historialMedico="";
+    }
+
+    public String getId() { return id; }
+    public void setId(String id){this.id=id; }
+
+    public String getNombre(){return nombre;}
+    public void setNombre(String nombre){this.nombre=nombre;}
+
+    public String getApellido(){return apellido;}
+    public void  setApellido(String apellido){this.apellido=apellido;}
+
+    public int getEdad(){return edad;}
+    public void setEdad(int edad){this.edad=edad;}
+
+    public String getGenero() {return genero;}
+    public void setGenero(String genero) {this.genero = genero;}
+
+    public String getDireccion() {return direccion;}
+    public void setDireccion(String direccion) {this.direccion = direccion;}
+
+
+
+}
