f015fcd6c569c6725a30921fe6cacde776781bd3
milton <miltonromero164@gmail.com>
2025-05-07 12:49:49 -0500

    Interfaz temporal

--- DatesEstructure/Hospital/Main.java
+++ DatesEstructure/Hospital/Main.java
@@ -1,5 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import Modelo.Doctor;
 import Modelo.Paciente;
 
 import javax.swing.JOptionPane;
@@ -8,9 +9,10 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        String[] opciones = {"Registrar paciente", "ver pacientes", "Salir"};
+        String[] opciones = {"Registrar paciente", "Ver pacientes","Salir"};
         int OpcSelec;
         Paciente nuevoPaciente = null;
+        Doctor doc = null;
 
         do {
             OpcSelec = JOptionPane.showOptionDialog(
@@ -25,43 +27,78 @@ public class Main {
 
             switch (OpcSelec) { ///registrar paciente
                 case 0:
-                    String id = JOptionPane.showInputDialog(null, "Ingrese el ID del paciente:", "Registro de Paciente", JOptionPane.PLAIN_MESSAGE);
-                    if (id != null && !id.trim().isEmpty()) {
-                        String nombre = JOptionPane.showInputDialog(null, "Ingrese el nombre del paciente:", "Registro de Paciente", JOptionPane.PLAIN_MESSAGE);
-                        if (nombre != null && !nombre.trim().isEmpty()) {
-                            String apellido = JOptionPane.showInputDialog(null, "Ingrese el apellido del paciente:", "Registro de Paciente", JOptionPane.PLAIN_MESSAGE);
-                            if (apellido != null && !apellido.trim().isEmpty()) {
-                                String edadStr = JOptionPane.showInputDialog(null, "Ingrese la edad del paciente:", "Registro de Paciente", JOptionPane.PLAIN_MESSAGE);
-                                try {
-                                    int edad = Integer.parseInt(edadStr);
-                                    String genero = JOptionPane.showInputDialog(null, "Ingrese el género del paciente:", "Registro de Paciente", JOptionPane.PLAIN_MESSAGE);
-                                    if (genero != null) {
-                                        String direccion = JOptionPane.showInputDialog(null, "Ingrese la dirección del paciente:", "Registro de Paciente", JOptionPane.PLAIN_MESSAGE);
-                                        if (direccion != null) {
-                                            String telefono = JOptionPane.showInputDialog(null, "Ingrese el teléfono del paciente:", "Registro de Paciente", JOptionPane.PLAIN_MESSAGE);
-                                            if (telefono != null) {
-                                                String historialMedico = JOptionPane.showInputDialog(null, "Ingrese el historial médico del paciente:", "Registro de Paciente", JOptionPane.PLAIN_MESSAGE);
-                                                if (historialMedico != null) {
-                                                    nuevoPaciente = new Paciente(id, nombre, apellido, edad, genero, direccion, telefono);
-                                                    JOptionPane.showMessageDialog(null, "Paciente con ID " + id + " registrado exitosamente.", "Registro Exitoso", JOptionPane.INFORMATION_MESSAGE);
 
+                    while (true) {
+                        String idD = JOptionPane.showInputDialog(null, "Ingrese el ID del doctor:", "Registro de doctor", JOptionPane.PLAIN_MESSAGE);
+                        if (idD != null && !idD.trim().isEmpty()) {
+                            String nombreD = JOptionPane.showInputDialog(null, "Ingrese el nombre del doctor:", "Registro de doctor", JOptionPane.PLAIN_MESSAGE);
+                            if (nombreD != null && !nombreD.trim().isEmpty()) {
+                                String apellidoD = JOptionPane.showInputDialog(null, "Ingrese el apellido del doctor:", "Registro de doctor", JOptionPane.PLAIN_MESSAGE);
+                                if (apellidoD != null && !apellidoD.trim().isEmpty()) {
+                                    String especialidad = JOptionPane.showInputDialog(null, "Ingrese la especialidad:", "Registro de doctor", JOptionPane.PLAIN_MESSAGE);
+                                    if (especialidad != null) {
+                                        String telefonoD = JOptionPane.showInputDialog(null, "Ingrese el teléfono del doctor:", "Registro de doctor", JOptionPane.PLAIN_MESSAGE);
+                                        if (telefonoD != null) {
+                                            doc = new Doctor(idD, nombreD, apellidoD, especialidad, telefonoD);
+                                            JOptionPane.showMessageDialog(null, "Doctor con ID " + idD + " registrado exitosamente.", "Registro Exitoso", JOptionPane.INFORMATION_MESSAGE);
+                                            break;
+                                        }
+                                    }
+                                } else if (apellidoD != null) {
+                                    JOptionPane.showMessageDialog(null, "El apellido no puede estar vacío.", "Error", JOptionPane.ERROR_MESSAGE);
+                                }
+                            } else if (nombreD != null) {
+                                JOptionPane.showMessageDialog(null, "El nombre no puede estar vacío.", "Error", JOptionPane.ERROR_MESSAGE);
+                            }
+                        } else if (idD != null) {
+                            JOptionPane.showMessageDialog(null, "La ID no puede estar vacio", "ERROR", JOptionPane.ERROR_MESSAGE);
+                        }
+                    }
+                    while (true) {
+                        String id = JOptionPane.showInputDialog(null, "Ingrese el ID del paciente:", "Registro de Paciente", JOptionPane.PLAIN_MESSAGE);
+                        if (id != null && !id.trim().isEmpty()) {
+                            String nombre = JOptionPane.showInputDialog(null, "Ingrese el nombre del paciente:", "Registro de Paciente", JOptionPane.PLAIN_MESSAGE);
+                            if (nombre != null && !nombre.trim().isEmpty()) {
+                                String apellido = JOptionPane.showInputDialog(null, "Ingrese el apellido del paciente:", "Registro de Paciente", JOptionPane.PLAIN_MESSAGE);
+                                if (apellido != null && !apellido.trim().isEmpty()) {
+                                    String edadStr = JOptionPane.showInputDialog(null, "Ingrese la edad del paciente:", "Registro de Paciente", JOptionPane.PLAIN_MESSAGE);
+                                    try {
+                                        int edad = Integer.parseInt(edadStr);
+                                        String genero = JOptionPane.showInputDialog(null, "Ingrese el género del paciente:", "Registro de Paciente", JOptionPane.PLAIN_MESSAGE);
+                                        if (genero != null) {
+                                            String direccion = JOptionPane.showInputDialog(null, "Ingrese la dirección del paciente:", "Registro de Paciente", JOptionPane.PLAIN_MESSAGE);
+                                            if (direccion != null) {
+                                                String telefono = JOptionPane.showInputDialog(null, "Ingrese el teléfono del paciente:", "Registro de Paciente", JOptionPane.PLAIN_MESSAGE);
+                                                if (telefono != null) {
+                                                    String historialMedico = JOptionPane.showInputDialog(null, "Ingrese el historial médico del paciente:", "Registro de Paciente", JOptionPane.PLAIN_MESSAGE);
+                                                    if (historialMedico != null) {
+                                                        nuevoPaciente = new Paciente(id, nombre, apellido, edad, genero, direccion, telefono);
+                                                        JOptionPane.showMessageDialog(null, "Paciente con ID " + id + " registrado exitosamente.", "Registro Exitoso", JOptionPane.INFORMATION_MESSAGE);
+                                                        break;
+                                                    }
                                                 }
                                             }
                                         }
-                                    }
 
-                                } catch (NumberFormatException e) {
-                                    JOptionPane.showMessageDialog(null, "La edad debe ser un número entero válido.", "Error", JOptionPane.ERROR_MESSAGE);
+                                    } catch (NumberFormatException e) {
+                                        JOptionPane.showMessageDialog(null, "La edad debe ser un número entero válido.", "Error", JOptionPane.ERROR_MESSAGE);
+                                    }
+                                } else if (apellido != null) {
+                                    JOptionPane.showMessageDialog(null, "El apellido del paciente no puede estar vacío.", "Error", JOptionPane.ERROR_MESSAGE);
                                 }
-                            } else if (apellido != null) {
-                                JOptionPane.showMessageDialog(null, "El apellido del paciente no puede estar vacío.", "Error", JOptionPane.ERROR_MESSAGE);
+                            } else if (nombre != null) {
+                                JOptionPane.showMessageDialog(null, "El nombre del paciente no puede estar vacío.", "Error", JOptionPane.ERROR_MESSAGE);
                             }
-                        } else if (nombre != null) {
-                            JOptionPane.showMessageDialog(null, "El nombre del paciente no puede estar vacío.", "Error", JOptionPane.ERROR_MESSAGE);
+                        } else if (id != null) {
+                            JOptionPane.showMessageDialog(null, "La ID del paciente no puede estar vacio", "ERROR", JOptionPane.ERROR_MESSAGE);
                         }
-                    } else if (id != null) {
-                        JOptionPane.showMessageDialog(null, "La ID del paciente no puede estar vacio", "ERROR", JOptionPane.ERROR_MESSAGE);
                     }
+
+
+
+
+
+
                 break;
                 case 1: //ver paciente
                     JOptionPane.showMessageDialog(null,nuevoPaciente.toString());
